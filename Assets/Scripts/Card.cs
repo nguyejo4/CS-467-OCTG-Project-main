045c126b0c06df9ea9d6bccd0afeb2c03b980123
nguyejo4 <59853790+nguyejo4@users.noreply.github.com>
2021-02-07 20:40:29 -0800

    Initial commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private cardCreator cardCreator;
+    [SerializeField] private GameObject back;
+    [SerializeField] private GameObject frame;
+    [SerializeField] private GameObject art;
+    private int _id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Vector3 startPos = this.transform.position;
+
+        //setting up the card assets for layered appearance
+        //back.transform.position = new Vector3(startPos.x, startPos.Y, (startPos.z - 10));
+        //frame.transform.position = new Vector3(startPos.x, startPos.Y, (startPos.z - 1));
+        //art.transform.position = new Vector3(startPos.x, startPos.Y, startPos.z);
+    }
+
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void setCardBack(Sprite image)
+    {
+        back.GetComponent<SpriteRenderer>().sprite = image;
+        //back.SetActive(false);
+    }
+
+    public void setCardFrame(int id, Sprite image)
+    {
+        _id = id;
+        frame.GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    public void setCardArt(int id, Sprite image)
+    {
+        _id = id;
+        art.GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    
+    public void OnMouseDown()
+    {
+        if (back.activeSelf)
+        {
+            back.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/cardCreator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardCreator : MonoBehaviour
+{
+    [SerializeField] private Card cardTemplate;
+    [SerializeField] private Sprite cardBack;
+    [SerializeField] private Sprite cardFrame;
+    [SerializeField] private Sprite[] cardArt;
+
+    //code idea use an array of IDs, shuffle, then procedurally generate cards
+    //public opponentDeckIDs = 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // original card position
+        Vector3 startPos = cardTemplate.transform.position;
+
+        // use an array of ints to create the players deck
+        int[] playerDeck = {0, 1, 2};
+        ShuffleDeck(playerDeck);
+
+        // loop over deck size
+        for (int i = 0; i < playerDeck.Length; i++)
+        {
+            //create new instance of card
+            Card newCard = Instantiate(cardTemplate) as Card;
+
+            // attach cardBack to cardTemplate
+            newCard.setCardBack(cardBack);
+
+            //attaching frame
+            //future code will have different frames for different types.
+            newCard.setCardFrame(playerDeck[i], cardFrame);
+
+            //attaching cardArt
+            newCard.setCardArt(playerDeck[i], cardArt[playerDeck[i]]);
+
+            // moving card onto board
+            newCard.transform.position = new Vector3(-10 + (4 * i), 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ShuffleDeck (int[] deck)
+    {
+        for (int i = 0; i < deck.Length; i++)
+        {
+            int tmp = deck[i];
+            int r = Random.Range(i, deck.Length);
+            deck[i] = r;
+            deck[r] = tmp;
+        }
+    }
+}
